4cb51cb17d017bcecc4b618af0baa78f35223108
Aberham Gebreselassie <Aberham.Gebreselassie@sherwin.com>
2021-08-07 23:19:47 -0400

    Update File

--- TestSln/TestProject/main.tf
+++ TestSln/TestProject/main.tf
@@ -17,7 +17,7 @@ location="EastUS2"
 }
 
 resource "azurerm_app_service_plan" "example" {
-  name                = "test-appserviceplan"
+  name                = "test-appservice-plan"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   app_service_plan_id = azurerm_app_service_plan.example.id
